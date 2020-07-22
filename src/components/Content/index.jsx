26326586ff25b2ffff5ec8477cd782d7d9dcbc93
patriciahudakova <p.hudakova@reply.com>
2020-07-22 15:09:30 +0100

    Content section complete

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Container, Col} from 'reactstrap'
 
 const Content = props => {
     return(
-        <>
-        </>
+        <div className="content">
+            <Container className="pt-xs-1 my-3 border">
+                    <p><br/><br/><br/><br/><br/><br/><br/><br/>
+                        <br/><br/><br/><br/><br/><br/><br/><br/>
+                        <br/><br/><br/><br/><br/><br/><br/><br/></p>
+            </Container>
+        </div>
     )
 }
 
