386c9a3edd1e6cd501e1b2431007ed9deec88321
wa-brown <68325359+wa-brown@users.noreply.github.com>
2020-07-22 10:28:07 +0100

    Init the Go backend

---
+++ src/backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/wa-brown/TODO/src/backend
+
+go 1.14
+
+require go.mongodb.org/mongo-driver v1.3.5
---
+++ src/backend/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// MongoClient to interact with mongoDB
+var MongoClient *mongo.Client
+
+func main() {
+	clientOptions := options.Client().ApplyURI("mongodb://mongo-service:27017")
+
+	MongoClient, _ = mongo.Connect(context.TODO(), clientOptions)
+
+	// Chekck the connection
+	err := MongoClient.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatalf("Cannot connect to mongo-service: %+v", err)
+	}
+
+}
