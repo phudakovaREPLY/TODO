2906b465f3e26d013d81df7c9774f887659ce84e
patriciahudakova <p.hudakova@reply.com>
2020-07-22 14:39:38 +0100

    Simple Navigation bar complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './assets/style.scss'
 import Navigation from "./components/Navigation";
-import Content from "./components/Navigation";
-import Footer from "./components/Navigation";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './assets/style.scss';
+import {Nav, NavbarBrand, Navbar, NavItem} from 'reactstrap';
+import logo from './assets/replyLogo.png';
 
 const Navigation = props => {
     return(
-        <>
-        </>
+        <div className='navigation'>
+            <Navbar className="navbar navbar-light bg-light">
+                <Nav>
+                    <NavItem>
+                        <a href="/">
+                            <img src={logo} height="33" width="33" alt="" /></a>
+                    </NavItem>
+                    <NavbarBrand>
+                        TODO
+                    </NavbarBrand>
+                </Nav>
+            </Navbar>
+        </div>
     )
 }
 
