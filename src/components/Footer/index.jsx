a093224170057dab1c96a3367255a048323bc528
patriciahudakova <p.hudakova@reply.com>
2020-07-22 13:41:21 +0100

    Created component skeleton, rendered by App.js

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Footer = props => {
     return(
-        <>
-        </>
+        <footer className="fixed-bottom">
+            Create
+        </footer>
     )
 }
-
-
 
 export default Footer
