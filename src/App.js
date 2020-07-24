bcfffc41ec2f0bf3426a6a74b678daf10e6373a1
patriciahudakova <p.hudakova@reply.com>
2020-07-24 09:43:38 +0100

    Merged Footer component into Content component and deleted redundant files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './assets/style.scss'
 import Navigation from "./components/Navigation";
 import Content from "./components/Content";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navigation />
       <Content />
-      <Footer />
     </>
   );
 }
